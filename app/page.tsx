c1f9fa194a68708530637117eb6403702860736b
harikapadia999 <harikapadia999@gmail.com>
2023-12-16 16:15:47 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+    </main>
+  );
+}
